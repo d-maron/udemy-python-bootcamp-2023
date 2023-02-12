0324e28e3b9c1fe7594d44b0906fb17951235387
Daniel Maron <daniel.maron@bravurasecurity.com>
2023-02-12 06:14:19 -0800

    pycodestyle corrections

--- tictactoe.py
+++ tictactoe.py
@@ -1,13 +1,14 @@
 # Tic Tac Toe game
 
-## Globals
+# Globals
 gameOn = True
 winner = None
-boardSpace = range(1,10)
+boardSpace = range(1, 10)
 currentPlayer = 0
 firstMove = 1
 
-currentBoard = [-1]*9   #Initialize board with empty cells
+currentBoard = [-1]*9   # Initialize board with empty cells
+
 
 def drawBoard(firstMove=0):
     print('\n' * 100)     # clear board
@@ -26,35 +27,37 @@ def drawBoard(firstMove=0):
 
     hLine = '---|---|---'
     board = ['',
-        f' {dispCells[6]} | {dispCells[7]} | {dispCells[8]} ', hLine,
-        f' {dispCells[3]} | {dispCells[4]} | {dispCells[5]} ', hLine,
-        f' {dispCells[0]} | {dispCells[1]} | {dispCells[2]} ', '']
+             f' {dispCells[6]} | {dispCells[7]} | {dispCells[8]} ', hLine,
+             f' {dispCells[3]} | {dispCells[4]} | {dispCells[5]} ', hLine,
+             f' {dispCells[0]} | {dispCells[1]} | {dispCells[2]} ', '']
 
     for row in board:
         print(row)
 
+
 def choosePiece():
     choice = ''
 
-    while choice.upper() not in ['X','O']:
+    while choice.upper() not in ['X', 'O']:
         choice = input("Player 1, please choose 'X' or 'O'.").upper()
     print()
     print(f"Player 1, your mark is '{choice}'. You will go first.")
 
     if choice == 'X':
-        return ('X','O')
+        return ('X', 'O')
     else:
-        return ('O','X')
+        return ('O', 'X')
 
 
 def chooseMove(player):
     choice = ''
     while choice not in boardSpace:
-        choice = input(f"Player {player+1}, choose where to put your mark (1-9) Type Q to quit.")
+        choice = input(f"Player {player+1}, "
+                       f"choose where to put your mark (1-9) Type Q to quit.")
         if choice.isdigit():
             choice = int(choice) - 1
             if choice+1 in boardSpace:
-                if currentBoard[choice] in range(0,2):
+                if currentBoard[choice] in range(0, 2):
                     print(f"Space {choice+1} is already marked.")
                 else:
                     choice = int(choice)
@@ -68,10 +71,11 @@ def chooseMove(player):
 
     currentBoard[choice] = player
 
+
 def isWon():
-    winPatterns = [[0,1,2],[3,4,5],[6,7,8],
-                   [0,3,6],[1,4,7],[2,5,8],
-                   [0,4,8],[2,4,6]]
+    winPatterns = [[0, 1, 2], [3, 4, 5], [6, 7, 8],     # Horizontals
+                   [0, 3, 6], [1, 4, 7], [2, 5, 8],     # Verticals
+                   [0, 4, 8], [2, 4, 6]]                # Diagonals
 
     for pattern in winPatterns:
         matchCount = 0
@@ -81,6 +85,7 @@ def isWon():
             if matchCount == 3:
                 return currentPlayer
     return None
+
 
 # Start the game
 chosenPieces = choosePiece()
@@ -94,12 +99,12 @@ while gameOn:
         break
     drawBoard()
     winner = isWon()
-    if winner != None:
+    if winner is not None:
         gameOn = False
     currentPlayer = abs(currentPlayer-1)
 
 # Game over
-if winner == None:
+if winner is None:
     print('Thank you for playing.')
 else:
     print(f'Player {winner+1} has won the game!')
