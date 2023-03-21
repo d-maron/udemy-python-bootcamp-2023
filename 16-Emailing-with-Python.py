99f09642dedfa37c43d1c86f605a61859e900af8
Daniel Maron <maron.daniel@gmail.com>
2023-03-21 09:45:13 -0400

    Finish email exercise

--- 16-Emailing-with-Python.py
+++ 16-Emailing-with-Python.py
@@ -1,20 +1,19 @@
 # For hidden passwords
-import getpass
-
-import smtplib
+import getpass, smtplib, imaplib
 
 smtp_object = smtplib.SMTP('smtp.gmail.com', 587)
 smtp_object.ehlo()
-
 smtp_object.starttls()
 
 email = getpass.getpass("Enter your email: ")
 password = getpass.getpass("Enter your password: ")
-smtp_object.login(email,password)
+smtp_object.login(email, password)
+
 from_address = email
 to_address = getpass.getpass("Enter the email of the recipient: ")
 subject = input("Enter the subject line: ")
 message = input("Type out the message you want to send: ")
 msg = "Subject: " + subject + '\n' + message
-smtp_object.sendmail(from_address,to_address,msg)
+smtp_object.sendmail(from_address, to_address, msg)
+
 smtp_object.quit()
