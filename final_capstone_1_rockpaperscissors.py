53be6bf387bb3599a42fdbdfd61fe089f6f4e0fb
Daniel Maron <maron.daniel@gmail.com>
2023-03-21 14:05:43 -0400

    Rock Paper Scissors: add score keeping

--- final_capstone_1_rockpaperscissors.py
+++ final_capstone_1_rockpaperscissors.py
@@ -1,12 +1,13 @@
 import random
 
 win_moves = [('ROCK', 'SCISSORS'), ('PAPER', 'ROCK'), ('SCISSORS', 'PAPER')]
+score = {'You': 0, 'Computer': 0}
 
 def player_move():
     choice = ''
     while not choice:
         choose_move = input('What is your move? (1 = ROCK, 2 = PAPER, 3 = SCISSORS, 4 = quit) ').lower()
-        if choose_move not in '1234':
+        if choose_move not in [str(i) for i in range(1,4)]:
             print('*** Please provide a valid input ***')
         else:
             choice = int(choose_move) - 1
@@ -27,18 +28,22 @@ while game_on:
         game_on = False
         break
     move = win_moves[moveidx][0]
-    print(f'Your move: {move} ({moveidx})')
+    print(f'Your move: {move}')
     compmoveidx = computer_move()
     compmove = win_moves[compmoveidx][0]
-    print(f'Computer\'s move: {compmove} ({compmoveidx})')
+    print(f'Computer\'s move: {compmove}')
 
     if move == compmove:
         print('IT\'S A TIE!')
-        continue
     elif compmove == win_moves[moveidx][1]:
         print('PLAYER wins!!')
+        score['You'] += 1
     else:
         print('COMPUTER wins!')
+        score['Computer'] += 1
 
+    print(f'SCORE:')
+    for i in score.keys():
+        print('  ', i, score[i])
 
 print ('GAME OVER')
